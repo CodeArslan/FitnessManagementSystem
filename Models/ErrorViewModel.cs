d5be08f04c02975cd6e7200014163a3b6bcc01b9
arslan5423 <sayhi.arslan>
2025-09-21 11:39:56 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
